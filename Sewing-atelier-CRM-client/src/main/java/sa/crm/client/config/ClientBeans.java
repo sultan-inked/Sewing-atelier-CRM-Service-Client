081f69b719595d6573ebad5e504c3ef6ec3450e0
Sultan <nikowosuka@gmail.com>
2024-07-20 14:30:53 +0500

    MOD: Configuring inter-service communication security 2

--- Sewing-atelier-CRM-client/src/main/java/sa/crm/client/config/ClientBeans.java
+++ Sewing-atelier-CRM-client/src/main/java/sa/crm/client/config/ClientBeans.java
@@ -3,6 +3,7 @@ package sa.crm.client.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestClient;
 
 import sa.crm.client.client.RestClientProductsRestClient;
@@ -12,22 +13,14 @@ public class ClientBeans {
 	
 	@Bean
 	public RestClientProductsRestClient productsRestClient(
-			@Value("${sa.service.crm.uri:http://localhost:8081}") String crmBaseUri) {
+			@Value("${sa.service.crm.uri:http://localhost:8081}") String crmBaseUri,
+			@Value("${sa.service.crm.username:}") String crmUsername,
+			@Value("${sa.service.crm.password:}") String crmPassword) {
 		
 		return new RestClientProductsRestClient(RestClient.builder()
 				.baseUrl(crmBaseUri)
+				.requestInterceptor(
+						new BasicAuthenticationInterceptor(crmUsername, crmPassword))
 				.build());
 	}
 }
-
-//@Configuration
-//public class ClientBeans {
-//
-//    @Bean
-//    public RestClientProductsRestClient productsRestClient(
-//            @Value("${selmag.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
-//        return new RestClientProductsRestClient(RestClient.builder()
-//                .baseUrl(catalogueBaseUri)
-//                .build());
-//    }
-//}
